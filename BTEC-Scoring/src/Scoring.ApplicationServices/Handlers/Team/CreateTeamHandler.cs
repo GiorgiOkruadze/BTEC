c388a0b4844c1aa013d027e954e06dec8e34b807
Giorgi Okruadze <41172610+GiorgiOkruadze@users.noreply.github.com>
2021-10-06 01:05:52 +0400

    changes

--- BTEC-Scoring/src/Scoring.ApplicationServices/Handlers/Team/CreateTeamHandler.cs
+++ BTEC-Scoring/src/Scoring.ApplicationServices/Handlers/Team/CreateTeamHandler.cs
@@ -5,6 +5,8 @@ using Scoring.CoreServices.Repositories.Abstractions;
 using Scoring.DatabaseModels.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,7 +25,16 @@ namespace Scoring.ApplicationServices.Handlers
 
         public async Task<bool> Handle(CreateTeamCommand request, CancellationToken cancellationToken)
         {
+            var teams = (await _teamRepository.GetAllAsync(new List<Expression<Func<Team, object>>>() { team => team.Students })).ToList();
+            var bigTeamsCount = teams.Where(team => team.Students.Count > 1).Count();
+            var smallTeamsCount = teams.Where(team => team.Students.Count == 1).Count();
             var students = _mapper.Map<List<Student>>(request.Students);
+
+            if(students.Count == 1 && smallTeamsCount>=20 || students.Count > 1 && bigTeamsCount >= 4)
+            {
+                return false;
+            }
+
             var team = new Team() { Students = students, TeamName = request.TeamName };
             return await _teamRepository.CreateAsync(team);
         }
